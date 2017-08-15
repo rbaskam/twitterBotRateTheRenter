3bded5bdb58249a838c45f9bb44649b51336513f
Robert Askam <rbaskam@gmail.com>
2017-08-15 12:37:02 +0100

    Testeing

--- bot.js
+++ bot.js
@@ -12,30 +12,35 @@ const Twit = require('./node_modules/twit');
 const TH = new Twit({ // Twit Handler
     consumer_key: 'KvtuhanMF4WYLfYprSrzCJlcV',
     consumer_secret: 'ZrVfxXxjUzz7rJNuWwiVfsfMpoRt9AM2pH6fnPN0kjEvyka9vD',
-    access_token: '	893498060663336964-mRDZqnc09mwxLoOL9RUX9ATMlsoIcGH',
+    access_token: '893498060663336964-mRDZqnc09mwxLoOL9RUX9ATMlsoIcGH',
     access_token_secret: '1o60UGZF8OqHbNvf89PvLhb9VFEz2w9a9tHWrOrHdaxPg'
 });
 
+const _self = {
+    id: 893498060663336964,
+    screen_name: 'ratetherenter'
+}
+
 const stream = TH.stream('statuses/filter', {
-    track: ['ratetherenter', 'www.ratetherenter.co.uk', 'ratetherenter.co.uk']
+    track: ['ratetherenter', 'ratetherenter.co.uk','www.ratetherenter.co.uk']
 });
 
-stream.on('tweet', (tweet) => {
-    console.log('Found Tweet');
+stream.on('tweet', (tweet) => {   
+    
     TH.post('favorites/create',{
         id:tweet.id_str
     },function (err,response) { 
-        console.log('Error');
     });
 
-    // TH.post('statuses/retweet/:id', {
-    //     id: tweet.id_str
-    // });
-    return; 
+    TH.post('statuses/retweet/:id', {
+        id: tweet.id_str
+    });   
 
-    // TH.post('statuses/update', {
-    //     status: `@${tweet.user.screen_name} Thanks for sharing :)`,
-    //     in_reply_to_status_id: tweet.id_str 
-    // });
+    TH.post('statuses/update', {
+        status: `@${tweet.user.screen_name} Thanks for sharing :)`,
+        in_reply_to_status_id: tweet.id_str 
+    });
+
+    return; 
     
 });
