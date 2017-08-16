6918155b4b232663e5d9ac7d03d5b3b3e1aee720
Robert Askam <rbaskam@gmail.com>
2017-08-16 15:00:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,10 +10,10 @@ app.get('/', function (request, response) {
 
 const Twit = require('./node_modules/twit');
 const TH = new Twit({ // Twit Handler
-     consumer_key: ENV['consumer_key'],
-    consumer_secret: ENV['consumer_secret'],
-    access_token: ENV['access_token'],
-    access_token_secret: ENV['access_token_secret']
+     consumer_key => ENV['consumer_key'],
+    consumer_secret => ENV['consumer_secret'],
+    access_token => ENV['access_token'],
+    access_token_secret => ENV['access_token_secret']
 });
 
 const _self = {
